e8a5811c9c1a26959df81a06bf0a53921fa4be39
yogesh000001 <yogesh.chauhan9548@gmail.com>
2025-03-04 13:55:40 +0530

    [Yogesh] Add. UC2

--- HelloGreetingApplication/Program.cs
+++ HelloGreetingApplication/Program.cs
@@ -1,10 +1,12 @@
 using BusinessLayer.Interface;
 using BusinessLayer.Service;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+logger.Info("Application is starting...");
 builder.Services.AddControllers();
 builder.Services.AddScoped<IGreetingBL, GreetingBL>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
